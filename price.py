8e21afe599f8f4dbb380dff088c85dfc511d2bcf
jjhcom <91904494+jjhcom@users.noreply.github.com>
2023-05-28 01:18:33 +0900

    Add files via upload

---
+++ price.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+def shopping(shop_file):
+  data_path = Path() / "data"
+  target_path = data_path / shop_file
+  shop_dict = {} # 생성할 사전 객체
+  with open(target_path, mode='r', encoding='utf-8') as f:
+    for i, line in enumerate(f):
+      if i >= 2:
+        product, price = line.split()
+        shop_dict[product] = price
+
+  return shop_dict
+
+def item_price(shop_file, item):
+    shop_dict = shopping(shop_file)
+    return shop_dict[item]
